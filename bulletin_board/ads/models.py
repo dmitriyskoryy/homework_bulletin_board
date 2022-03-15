92d9d207873bd3c05a6fefadc83ed0c02a3102f1
Dmitriy Skoryy <dmitriyskoryy@gmail.com>
2022-03-15 20:30:21 +0200

    add models Author, Category, Adt, Respond, Subscriber

--- bulletin_board/ads/models.py
+++ bulletin_board/ads/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+
+class Author(models.Model):
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.authorUser.username}'
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+class Adt(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Author')
+    adtCategory = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+
+    dateCreation = models.DateTimeField(auto_now_add=True, verbose_name='Date')
+    title = models.CharField(max_length=128, verbose_name='Caption')
+    text = models.TextField(verbose_name='Text adt')
+    rating = models.SmallIntegerField(default=0)
+
+
+
+
+class Respond(models.Model):
+    responseAdt = models.ForeignKey(Adt, on_delete=models.CASCADE)
+    # комментарии могут оставлять все, поэтому связь с User, а не с Author
+    responseUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    #получил отклик принятие или нет
+    acceptResponse = models.BooleanField(default=False)
+
+
+
+
+
+class Subscriber(models.Model):
+    subscribersUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    adtCategory = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.subscribersUser} {self.adtCategory}'
+
